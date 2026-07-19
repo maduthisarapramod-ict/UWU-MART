4ae4b6a2e201f637c2c0a6dccf12e3613b982482
maduthisarapramod-ict <maduthisarapramod@gmail.com>
2026-07-19 15:59:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const ItemSchema = new mongoose.Schema({
     title: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
-    location: { type: String, required: true },
+    location: { type: String }, // Location Optional කරන ලදී
     whatsapp: { type: String, required: true },
     description: { type: String },
     imageUrl: { type: String },
